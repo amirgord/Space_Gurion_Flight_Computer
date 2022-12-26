733ee42a767c84c418797b86e10b679adcb772c8
Amir Gordon <Amir.gordon1@gmail.com>
2022-12-26 13:12:06 +0200

    Add existing file

---
+++ src/main.py
@@ -0,0 +1,27 @@
+# import "bit/"
+# import "parachute/Parachute.py"
+# import "sensor/Sensor.py"
+# import "radio/Radio.py"
+# import "../log/Logger.py"
+
+def main():
+    #Initilize sesors & write to looger: -------------
+    # Parachute parachute
+    # Sensor sensor
+    # Radio radio
+    # Logger logger
+    
+    
+    #BIT & write to looger: ----------
+    
+    
+    #Main loop:
+    while True:
+        #get data from sensors & write to looger: ---------
+        
+        #open parachute & write to looger: ------------
+        
+        #send data & write to looger: --------
+        break
+        
+
